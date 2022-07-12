3115576bc54b79730470a07b24feb3d93cc81d0a
krllps <renat.kyryllov@gmail.com>
2022-07-13 00:31:21 +0300

    Alter user, task tables
    Added registered_on, data_of_birth columns to user model.
    Added created_on column to task model.
    Added respective migration

--- api/app/models.py
+++ api/app/models.py
@@ -9,9 +9,11 @@ class User(Base):
     __mapper_args__ = {"eager_defaults": True}  # required in order to access columns with server defaults
 
     id = sqla.Column(sqla.Integer, primary_key=True)
-    name = sqla.Column(sqla.String, nullable=False, unique=True, index=True)
-    email = sqla.Column(sqla.String, default=None, unique=True, index=True)
-    password = sqla.Column(sqla.String, nullable=False)
+    name = sqla.Column(sqla.String(50), nullable=False, unique=True, index=True)
+    email = sqla.Column(sqla.String(100), nullable=True, default=None, unique=True, index=True)
+    password = sqla.Column(sqla.String(100), nullable=False)
+    registered_on = sqla.Column(sqla.DateTime, nullable=False, server_default=sqla.func.now())
+    data_of_birth = sqla.Column(sqla.Date, nullable=True, default=None)
 
     tasks = relationship("Task", backref="user")
 
@@ -24,10 +26,11 @@ class Task(Base):
     __mapper_args__ = {"eager_defaults": True}  # required in order to access columns with server defaults
 
     id = sqla.Column(sqla.Integer, primary_key=True)
-    name = sqla.Column(sqla.String, nullable=False, index=True)
-    description = sqla.Column(sqla.String, default=None)
+    name = sqla.Column(sqla.String(100), nullable=False, index=True)
+    description = sqla.Column(sqla.String(300), nullable=True, default=None)
+    created_on = sqla.Column(sqla.DateTime, nullable=True, server_default=sqla.func.now())
     due = sqla.Column(sqla.DateTime, nullable=False, index=True)
-    is_checked = sqla.Column(sqla.Boolean, default=False, index=True)
+    is_checked = sqla.Column(sqla.Boolean, nullable=False, default=False, index=True)
 
     user_id = sqla.Column(sqla.Integer, sqla.ForeignKey("user.id"))
 
